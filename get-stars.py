a71fffc115852f9c987ed43fa5af1ee85a1ef974
Camila C. A <cycuesta@gmail.com>
2020-05-19 20:48:19 -0500

    gets files i like

--- get-stars.py
+++ get-stars.py
@@ -1,5 +1,5 @@
-# Import libraries
-# https://python-forum.io/Thread-Web-Scraping-part-1
+# by cycuestaa
+import os
 import requests
 import urllib.request
 import time
@@ -13,18 +13,36 @@ response = requests.get(url)
 # parse html
 soup = BeautifulSoup(response.text, "html.parser")
 
-# locate all <a> tags
+# variables 
+dest = "./starimgs/"    # files save here. make sure ends with /
+obj_name = "M3_i-band"
+file_type = "FCAL.fits"
+obj_len = len(obj_name)
+ft_len = len(file_type)
 count = 1 
-url_len = len(url)
 
+## CREATE directory, if doesnt exist
+path = os.getcwd()
+print("Detected working dir: %s" % path)
+try:
+    os.mkdir(dest)
+except OSError:
+    print ("CREATE failed. dir: %s likely exists" % path)
+else:
+    print ("CREATE done. files will save into dir: %s" % path)
+
+
+## GET & Download
 for a_tag in soup.findAll('a'):  #'a' tags are for links
     if count >= 6 : # line for first file listed
         link = a_tag['href']
-#        test += 1
-        print(count, link)
-        download_url = url + link
-        urllib.request.urlretrieve(download_url,'./imgs/'+link) 
-        time.sleep(1) # pause to not break 
+
+        if(link[:obj_len]==obj_name)&(link[-ft_len:]==file_type):
+            print(link)
+            download_url = url + link
+            urllib.request.urlretrieve(download_url,dest+link) 
+            time.sleep(1) # pause to not break 
+
     count += 1
 
 
